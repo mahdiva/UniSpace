bfd3d939d2c16c9b414ab90b76a62466aa1bf551
exclu <exclusive.vadymkopko@gmail.com>
2021-02-13 17:07:10 -0700

    Setup a connection to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+require("dotenv").config();
+const mongoose = require("mongoose");
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true });
+
 app.get("/", (req, res) => {
   res.send("<h1>UniSpace!</h1>");
   console.log("Get Request: /");
