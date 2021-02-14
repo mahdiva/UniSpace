96dd89301456f2333e8b47349627b496df9853a8
exclu <exclusive.vadymkopko@gmail.com>
2021-02-13 18:33:57 -0700

    Attempt to query MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,54 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
+async function main() {
+  const express = require("express");
+  const app = express();
+  const PORT = 3000;
 
-require("dotenv").config();
-const mongoose = require("mongoose");
-const uri = process.env.MONGODB_URI;
+  require("dotenv").config();
+  const mongoose = require("mongoose");
+  const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true });
+  // Connect to the MongoDB
+  await mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Successfully connected to the database...");
+    })
+    .catch(() => {
+      console.log("Failed to connect to the database...");
+      console.log(err);
+    });
 
-app.get("/", (req, res) => {
-  res.send("<h1>UniSpace!</h1>");
-  console.log("Get Request: /");
-});
+  // Creating a Schema
+  const testSchema = new mongoose.Schema({
+    name: String,
+    beds: Number,
+    first_review: Date,
+  });
 
-app.listen(PORT, () => {
-  console.log(`Listening at http://localhost:${PORT}`);
-});
+  /* Creata a new Schema
+  const shcoolSchema = new mongoose.Schema({
+      // schoolID: { type: int, unique: true }
+      schoolName: string
+  });
+
+  const School = mongoose.model('School',shcoolSchema );
+  const uOfC = new School({schoolName: 'University of Calgary'})
+  */
+
+  // Find a table
+  const listingsAndReviews = mongoose.model("ListingsAndReview", testSchema);
+  await listingsAndReviews.find({}).then((data) => console.log(data));
+
+  app.get("/", (req, res) => {
+    res.send("<h1>UniSpace!</h1>");
+    console.log("Get Request: /");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+  });
+}
+main();
